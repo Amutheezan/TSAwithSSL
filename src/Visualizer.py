651cb9ade20f9e1e78dcb1f15a0d61026a4310f4
Amutheezan <amux93@yahoo.com>
2017-10-22 11:06:25 +0530

    Updated GUI

--- src/Visualizer.py
+++ src/Visualizer.py
@@ -1,4 +1,5 @@
 import sys
+
 from Training import *
 from SystemStore import *
 from PyQt4 import QtGui , QtCore
@@ -51,8 +52,6 @@ class Visualizer(QtGui.QMainWindow):
                 self.method.predict(tweet)))
         else:
             self.prediction.setText("No Model Available")
-            self._generate_model_()
-            self._predict_tweet_()
 
     def __init__(self):
         super(Visualizer , self).__init__()
@@ -66,6 +65,7 @@ class Visualizer(QtGui.QMainWindow):
         self.training_type_selector.insertItems(1,self.cons.TRAINING_TYPES)
         self.training_type_selector.move(60,50)
         self.training_type_selector.resize(200, 30)
+        self.training_type_selector.setStyleSheet("background-color: white")
         self.label_tts = QtGui.QLabel(self)
         self.label_tts.setText("Select the Test Type")
         self.label_tts.move(335 , 20)
@@ -74,6 +74,7 @@ class Visualizer(QtGui.QMainWindow):
         self.test_type_selector.insertItems(1,self.cons.TEST_TYPES)
         self.test_type_selector.move(335, 50)
         self.test_type_selector.resize(220, 30)
+        self.test_type_selector.setStyleSheet("background-color: white")
         self.label_llt = QtGui.QLabel(self)
         self.label_llt.setText("Label Limit")
         self.label_llt.move(60 , 100)
@@ -81,6 +82,7 @@ class Visualizer(QtGui.QMainWindow):
         self.label_limit_text.move(160 , 100)
         self.label_limit_text.setText(str(10000))
         self.label_limit_text.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.label_limit_text.setStyleSheet("background-color: white")
         self.label_llt2 = QtGui.QLabel(self)
         self.label_llt2.setText("Un Label Limit")
         self.label_llt2.move(335 , 100)
@@ -88,6 +90,7 @@ class Visualizer(QtGui.QMainWindow):
         self.un_label_limit_text.move(460 , 100)
         self.un_label_limit_text.setText(str(20000))
         self.un_label_limit_text.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.un_label_limit_text.setStyleSheet("background-color: white")
         self.label_llt3 = QtGui.QLabel(self)
         self.label_llt3.setText("Test Limit")
         self.label_llt3.move(60 , 140)
@@ -95,6 +98,7 @@ class Visualizer(QtGui.QMainWindow):
         self.test_limit.move(160 , 140)
         self.test_limit.setText(str(5000))
         self.test_limit.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.test_limit.setStyleSheet("background-color: white")
         self.label_llt2 = QtGui.QLabel(self)
         self.label_llt2.setText("No of Iteration")
         self.label_llt2.move(335 , 140)
@@ -102,27 +106,30 @@ class Visualizer(QtGui.QMainWindow):
         self.no_of_iteration.move(460 , 140)
         self.no_of_iteration.setText(str(10))
         self.no_of_iteration.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.no_of_iteration.setStyleSheet("background-color: white")
         self.generate_model = QtGui.QPushButton("Generate Model",self)
         self.generate_model.move(225 , 190)
         self.generate_model.resize(180, 50)
         self.generate_model.clicked.connect(lambda : self._generate_model_())
-        self.generate_model.setStyleSheet("background-color:#383a39; color: #a1dbcd;")
+        self.generate_model.setStyleSheet("background-color:#383a39; color: #1dcaff;")
         self.tweet_input = QtGui.QLineEdit(self)
         self.tweet_input.move(60 , 260)
         self.tweet_input.resize(500, 30)
         self.tweet_input.setText("Welcome to Twitter!!!")
-        self.tweet_input.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.tweet_input.setStyleSheet("background-color: white")
+        self.tweet_input.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         self.predict_tweet = QtGui.QPushButton("Predict Tweet",self)
         self.predict_tweet.move(225 , 310)
         self.predict_tweet.resize(180, 50)
         self.predict_tweet.clicked.connect(lambda : self._predict_tweet_())
-        self.predict_tweet.setStyleSheet("background-color: #383a39; color: #a1dbcd;")
+        self.predict_tweet.setStyleSheet("background-color: #383a39; color: #1dcaff;")
         self.prediction = QtGui.QLabel(self)
         self.prediction.setText("No Prediction")
         self.prediction.move(30 , 360)
         self.setGeometry(375 , 200  , 620  , 400)
-        self.setWindowTitle('TSAwithSSL, v0.1.2.3')
-        self.setStyleSheet("background-color: #a1dbcd; color: #383a39;")
+        self.setWindowTitle('TSAwithSSL, v0.1.2.3_1')
+        self.setStyleSheet("background-color: #1dcaff; color: #383a39;")
+        self.setWindowIcon(QtGui.QIcon('../resource/images/icons/ico.png'))
         self.show()
 
 
